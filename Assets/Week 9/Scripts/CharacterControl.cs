b1ced273cb7b3ab79b73a35f45b5d8379902a6b0
LAPTOP-OF-ALEX\Alex <carrasal@sheridancollege.ca>
2024-03-14 15:28:56 -0400

    Task 4b complete: UI now shows selected villager type

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,10 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+
+    public static TextMeshProUGUI villagerType;
+
+
+    public void Start()
+    {
+        villagerType = FindObjectOfType<TextMeshProUGUI>();
+    }
+
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -14,6 +24,10 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        villagerType.text = SelectedVillager.GetType().Name;
+
+
+
     }
-    
+
 }
