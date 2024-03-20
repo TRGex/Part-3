f4626fca5979dc84832c1b2efc28581ee84678f7
LAPTOP-OF-ALEX\Alex <carrasal@sheridancollege.ca>
2024-03-20 10:08:57 -0400

    Task 1 complete: Dashing now works with coroutines

--- Assets/Week 9/Scripts/Thief.cs
+++ Assets/Week 9/Scripts/Thief.cs
@@ -7,30 +7,47 @@ public class Thief : Villager
     public GameObject daggerPrefab;
     public Transform spawnPoint1;
     public Transform spawnPoint2;
+    public float dashSpeed = 7;
+    Coroutine dashing;
+
     public override ChestType CanOpen()
     {
         return ChestType.Thief;
     }
 
-    protected override void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            speed = 3;
-        }
-        base.Update();
+    //protected override void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        speed = 3;
+    //    }
+    //    base.Update();
 
-    }
+    //}
 
     protected override void Attack()
     {
-        speed = 12;
         destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        base.Attack();
-        Instantiate(daggerPrefab, spawnPoint1.position, spawnPoint1.rotation);
-        Instantiate(daggerPrefab, spawnPoint2.position, spawnPoint2.rotation);
-
+        if(dashing != null)
+        {
+            StopCoroutine(dashing);
+        }
+        dashing = StartCoroutine(Dash());
     }
 
-    
+    IEnumerator  Dash()
+    {
+        speed += dashSpeed;
+        while (speed > 3)
+        {
+            yield return null;
+        }
+        base.Attack();
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(daggerPrefab, spawnPoint1.position, spawnPoint1.rotation);
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(daggerPrefab, spawnPoint2.position, spawnPoint2.rotation);
+    }
+
+
 }
